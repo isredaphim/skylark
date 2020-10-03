1660e0bd83e5a1c863814ce73385f7c0f8bd5b25
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-10-03 21:09:25 +0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,59 @@
+
+<html>
+	<head> <title>Skylark</title>
+	<link rel="stylesheet" href="styles1.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+	
+	<div id="tops"> 
+	
+		
+		<div id="logo"> 
+		
+			<div id="triangle-up"></div>
+			<div id="triangle-topleft"></div>
+			<div id="triangle-topright"></div>
+			<div id="triangle-bottomleft"></div>
+			<div id="triangle-bottomright"></div>
+
+		</div>
+		
+		<div id="comname"><b>The Skylark</b></div>
+		
+		<div class="topnav">
+		
+			<b><a class="active" href="">Home</a>
+			<a href="">News</a>
+			<a href="">Contact</a>
+			<a href="">About</a></b>
+			 <input type="text" placeholder="Search..">
+			
+		</div>
+		
+	</div>
+	
+	<div id="mids"> 
+	
+		<div id="arrow"> </div>
+		<div id="fbox"><div id="txtf"> AUSTRALIA BOREALIS</div></div>
+		<div id="sbox"><div id="txts"> ICELAND BOREALIS</div></div>
+		<div id="tbox"><div id="txtt"> GREENLAND BOREALIS</div></div>
+		<div id="arrow2"> </div>
+	
+	</div>
+
+	
+	
+	<div id="bots">
+		<div id="chans"> 
+			<a href="#" class="fa fa-facebook"></a>
+			<a href="#" class="fa fa-twitter"></a>
+			<a href="#" class="fa fa-google"></a>
+		</div>
+	</div>
+	
+		
+
+	</body>
+</html>
